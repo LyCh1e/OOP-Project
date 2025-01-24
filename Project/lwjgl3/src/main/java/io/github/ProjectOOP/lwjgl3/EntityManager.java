a9b9018c59929e458ebbf55433a34de46469fbe9
Cyube <lcslayer22@gmail.com>
2025-01-24 18:42:34 +0800

    changes

--- Project/lwjgl3/src/main/java/io/github/ProjectOOP/lwjgl3/EntityManager.java
+++ Project/lwjgl3/src/main/java/io/github/ProjectOOP/lwjgl3/EntityManager.java
@@ -1,5 +1,18 @@
 package io.github.ProjectOOP.lwjgl3;
 
-public class EntityManager {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class EntityManager {
+private List<Entity> entityList = new ArrayList<>();
+	
+	void addEntities(Entity e) {
+		entityList.add(e);
+	}
+	
+	void draw (SpriteBatch batch) {
+		
+	}
 }
--- Project/lwjgl3/src/main/java/io/github/ProjectOOP/lwjgl3/GameMaster.java
+++ Project/lwjgl3/src/main/java/io/github/ProjectOOP/lwjgl3/GameMaster.java
@@ -1,5 +1,24 @@
 package io.github.ProjectOOP.lwjgl3;
 
-public class GameMaster {
+import java.util.Random;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class GameMaster extends ApplicationAdapter{
+	public void create() {
+		
+	}
+	
+	public void render () {
+		
+	}
+	
+	public void dispose() {
+		
+	}
+	
 }
