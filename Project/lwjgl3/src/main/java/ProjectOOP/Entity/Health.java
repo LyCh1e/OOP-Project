8c2009ffebf09ccd137103f6d23370cd7cb0ad27
Nathan <natliong658@gmail.com>
2025-03-07 14:02:22 +0800

    Updated Health.java

--- Project/lwjgl3/src/main/java/ProjectOOP/Entity/Health.java
+++ Project/lwjgl3/src/main/java/ProjectOOP/Entity/Health.java
@@ -1,8 +1,14 @@
 package ProjectOOP.Entity;
 
-public class Health extends ImmovableEntity{
+public class Health extends ImmovableEntity {
+    private int healthValue;
 
-	public Health() {
-		
-	}
+    public Health(float x, float y) {
+        super("heart.png", x, y, 0); // Image of heart
+        this.healthValue = 1; // Each heart represents 1 health point
+    }
+
+    public int getHealthValue() {
+        return healthValue;
+    }
 }
